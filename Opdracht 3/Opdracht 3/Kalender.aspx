7f540d40968fbcf3226891906753fd990c2d89f0
JurgenBurggraaff <j.burggraaff99@gmail.com>
2017-03-19 20:42:20 +0100

    Opdracht 3

---
+++ Opdracht 3/Opdracht 3/Kalender.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Kalender.aspx.cs" Inherits="Opdracht_3.Kalender" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Calendar ID="kalVerjaardag" runat="server" OnSelectionChanged="kalVerjaardag_SelectionChanged"></asp:Calendar>
+        <br />
+        <asp:Label ID="lbl1" runat="server" Text="U heeft de onderstaande datum geselecteerd:"></asp:Label>
+    
+    </div>
+        <p>
+            <asp:Label ID="lbl2" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:Label ID="lbl3" runat="server" Text="U bent nu"></asp:Label>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:Button ID="btnLeeftijd" runat="server" OnClick="btnLeeftijd_Click" Text="Laat zien" />
+    </form>
+</body>
+</html>
---
+++ Opdracht 3/Opdracht 3/Kalender.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Opdracht_3
+{
+    public partial class Kalender : System.Web.UI.Page
+    {
+        private DateTime verjaardag;
+        private string verjaardagText = "Mijn geboortedatum is op:";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                // Dit is geen postback maar de eerste keer dat je webpagina wort geladen.
+                // We zetten de kalender op 1-jan-1995
+                verjaardag = new DateTime(1995, 1, 1);
+                kalVerjaardag.VisibleDate = verjaardag; 
+            }
+        }
+
+        protected void kalVerjaardag_SelectionChanged(object sender, EventArgs e)
+        {
+            verjaardag = kalVerjaardag.SelectedDate;
+            lbl1.Text = verjaardagText + verjaardag.ToLongDateString();
+
+            DateTime nu = DateTime.Now;
+            TimeSpan verschil = nu.Subtract(verjaardag);
+            int jaren = verschil.Days / 365;
+        }
+
+        protected void btnLeeftijd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
