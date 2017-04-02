f525168d2ed89b7e4f26ba7d6a5918737067bac1
JurgenBurggraaff <j.burggraaff99@gmail.com>
2017-04-02 19:08:29 +0200

    Opdracht 6

---
+++ Opdracht 6/GrafischeRekenMachine/GrafischeRekenMachine/rekenmachine.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="rekenmachine.aspx.cs" Inherits="GrafischeRekenMachine.rekenmachine" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            De rekenmachine<br />
+            <br />
+            Hieronder staat een grafische rekenmachine. Druk op de knoppen om deze te gebruiken.<br />
+            <br />
+            <asp:TextBox ID="txtScherm" runat="server" Height="40px" Width="228px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btn7" runat="server" Height="40px" Text="7" Width="40px" OnClick="btn7_Click" />
+            &nbsp;<asp:Button ID="btn8" runat="server" Height="40px" Text="8" Width="40px" OnClick="btn8_Click" />
+            &nbsp;<asp:Button ID="btn9" runat="server" Height="40px" Text="9" Width="40px" OnClick="btn9_Click" />
+            &nbsp;<asp:Button ID="btnC" runat="server" Height="40px" Text="C" Width="40px" OnClick="btnC_Click" />
+            &nbsp;<asp:Button ID="btnBack" runat="server" Height="40px" Text="Back" Width="40px" OnClick="btnBack_Click" />
+            <br />
+            <asp:Button ID="btn4" runat="server" Height="40px" Text="4" Width="40px" OnClick="btn4_Click" />
+            &nbsp;<asp:Button ID="btn5" runat="server" Height="40px" Text="5" Width="40px" OnClick="btn5_Click" />
+            &nbsp;<asp:Button ID="btn6" runat="server" Height="40px" Text="6" Width="40px" OnClick="btn6_Click" />
+            &nbsp;<asp:Button ID="btnPlus" runat="server" Height="40px" Text="+" Width="40px" OnClick="btnPlus_Click" />
+            &nbsp;<asp:Button ID="btnM2" runat="server" Height="40px" Text="M2" Width="40px" OnClick="btnM2_Click" />
+            <br />
+            <asp:Button ID="btn1" runat="server" Height="40px" Text="1" Width="40px" OnClick="btn1_Click" />
+            &nbsp;<asp:Button ID="btn2" runat="server" Height="40px" Text="2" Width="40px" OnClick="btn2_Click" />
+            &nbsp;<asp:Button ID="btn3" runat="server" Height="40px" Text="3" Width="40px" OnClick="btn3_Click" />
+            &nbsp;<asp:Button ID="btnMin" runat="server" Height="40px" Text="-" Width="40px" OnClick="btnMin_Click" />
+            &nbsp;<asp:Button ID="btnO1" runat="server" Height="40px" Text="O1" Width="40px" />
+            <br />
+            <asp:Button ID="btn0" runat="server" Height="40px" Text="0" Width="88px" OnClick="btn0_Click" />
+            &nbsp;<asp:Button ID="btnPunt" runat="server" Height="40px" Text="." Width="40px" OnClick="btnPunt_Click" />
+            &nbsp;<asp:Button ID="btnIs" runat="server" Height="40px" Text="=" Width="40px" OnClick="btnIs_Click" />
+            &nbsp;<asp:Button ID="btnO2" runat="server" Height="40px" Text="O2" Width="40px" />
+
+            <br />
+            <asp:Label ID="lblGetal1" runat="server" Visible="False"></asp:Label>
+            <asp:Label ID="lblBereken" runat="server" Visible="False"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Opdracht 6/GrafischeRekenMachine/GrafischeRekenMachine/rekenmachine.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GrafischeRekenMachine
+{
+    public partial class rekenmachine : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length == 0)
+            {
+                txtScherm.Text += "";
+            }
+            else
+            {
+                txtScherm.Text += "0";
+            }
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            //Controleer of er minder dan 7 tekens staan
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "1";
+            }
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "2";
+            }
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "3";
+            }
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "4";
+            }
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "5";
+            }
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "6";
+            }
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "7";
+            }
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "8";
+            }
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length < 7)
+            {
+                txtScherm.Text += "9";
+            }
+        }
+
+        protected void btnPunt_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Contains(",") != true)
+            {
+                if (txtScherm.Text.Length == 0)
+                {
+                    txtScherm.Text += "0,";
+                }
+                else
+                {
+                    txtScherm.Text += ",";
+                }
+            }
+        }
+
+        protected void btnPlus_Click(object sender, EventArgs e)
+        {
+            lblGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            lblBereken.Text = "+";
+        }
+
+        protected void btnIs_Click(object sender, EventArgs e)
+        {
+            //Plaats getallen in variabelen
+            double G1 = Convert.ToDouble(txtScherm.Text);
+            double G2 = Convert.ToDouble(lblGetal1.Text);
+            double Uitkomst = 0;
+            //Maak berekening
+            if (lblBereken.Text == "+")
+            {
+                Uitkomst = G1 + G2;
+            }
+            if (lblBereken.Text == "-")
+            {
+                Uitkomst = G2 - G1;
+            }
+            //Plaats berekening in scherm
+            txtScherm.Text = Uitkomst.ToString();
+        }
+
+        protected void btnC_Click(object sender, EventArgs e)
+        {
+            txtScherm.Text = "";
+            lblBereken.Text = "";
+            lblGetal1.Text = "";
+        }
+
+        protected void btnM2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            if (txtScherm.Text.Length > 0)
+            {
+                txtScherm.Text = txtScherm.Text.Remove(txtScherm.Text.Length - 1, 1);
+            }
+        }
+
+        protected void btnMin_Click(object sender, EventArgs e)
+        {
+            lblGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            lblBereken.Text = "-";
+        }
+    }
+}
