4da3b3390a1a9751b39c7c0271f1d1eb97ba3589
JurgenBurggraaff <j.burggraaff99@gmail.com>
2017-04-20 14:25:15 +0200

    Opdracht 7

---
+++ Opdracht 7/Opdracht7/Opdracht7/Opdracht7.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Opdracht7.aspx.cs" Inherits="Opdracht7.Opdracht7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Rekentoets</title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>
+                Tafeltester
+            </h1>
+            <br />
+            <p>
+                Vul het hoogste getal in dat gebruikt mag worden in de sommen: <asp:TextBox ID="txtInvoer" runat="server"></asp:TextBox>
+            </p>
+            &nbsp;
+            <br />
+            <asp:Button ID="btnMaak" runat="server" Text="Maak sommen" OnClick="btnMaak_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblSommen" runat="server" Text="Sommen:"></asp:Label>
+            <br />
+            <br />
+            1 x
+            <asp:Label ID="lblSom1" runat="server"></asp:Label>
+            =&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtUitkomst1" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;
+            &nbsp;<asp:Label ID="lblResultaat1" runat="server"></asp:Label>
+            <br />
+            2 x
+            <asp:Label ID="lblSom2" runat="server"></asp:Label>
+            =&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtUitkomst2" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="lblResultaat2" runat="server"></asp:Label>
+            <br />
+            3 x
+            <asp:Label ID="lblSom3" runat="server"></asp:Label>
+            =&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtUitkomst3" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="lblResultaat3" runat="server"></asp:Label>
+            <br />
+            4 x
+            <asp:Label ID="lblSom4" runat="server"></asp:Label>
+            =&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtUitkomst4" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="lblResultaat4" runat="server"></asp:Label>
+            <br />
+            5 x
+            <asp:Label ID="lblSom5" runat="server"></asp:Label>
+            =&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtUitkomst5" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="lblResultaat5" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnScore" runat="server" Text="Laat score zien" OnClick="btnScore_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblText" runat="server" Text="Je hebt een"></asp:Label>
+            &nbsp;<asp:Label ID="lblCijfer" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Opdracht 7/Opdracht7/Opdracht7/Opdracht7.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Opdracht7
+{
+    public partial class Opdracht7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMaak_Click(object sender, EventArgs e)
+        {
+            int Invoer;
+            Invoer = Convert.ToInt16(txtInvoer.Text);
+
+            Random generator = new Random();
+            int getal1 = generator.Next(Invoer) + 1;
+            int getal2 = generator.Next(Invoer) + 1;
+            int getal3 = generator.Next(Invoer) + 1;
+            int getal4 = generator.Next(Invoer) + 1;
+            int getal5 = generator.Next(Invoer) + 1;
+
+            lblSom1.Text = getal1.ToString() ;
+            lblSom2.Text = getal2.ToString() ;
+            lblSom3.Text = getal3.ToString() ;
+            lblSom4.Text = getal4.ToString() ;
+            lblSom5.Text = getal5.ToString() ;
+
+        }
+
+        protected void btnScore_Click(object sender, EventArgs e)
+        {
+            int cijfer = 10;
+
+            int som1 = 1 * (Convert.ToInt16(lblSom1.Text));
+            int som2 = 2 * (Convert.ToInt16(lblSom2.Text));
+            int som3 = 3 * (Convert.ToInt16(lblSom3.Text));
+            int som4 = 4 * (Convert.ToInt16(lblSom4.Text));
+            int som5 = 5 * (Convert.ToInt16(lblSom5.Text));
+
+            if (som1 == Convert.ToInt16(txtUitkomst1.Text))
+            {
+                lblResultaat1.Text = "Goed";
+            }
+            else
+            {
+                lblResultaat1.Text = "Fout";
+                cijfer -= 2;
+            }
+
+            if (som2 == Convert.ToInt16(txtUitkomst2.Text))
+            {
+                lblResultaat2.Text = "Goed";
+            }
+            else
+            {
+                lblResultaat2.Text = "Fout";
+                cijfer -= 2;
+            }
+
+            if (som3 == Convert.ToInt16(txtUitkomst3.Text))
+            {
+                lblResultaat3.Text = "Goed";
+            }
+            else
+            {
+                lblResultaat3.Text = "Fout";
+                cijfer -= 2;
+            }
+
+            if (som4 == Convert.ToInt16(txtUitkomst4.Text))
+            {
+                lblResultaat4.Text = "Goed";
+            }
+            else
+            {
+                lblResultaat4.Text = "Fout";
+                cijfer -= 2;
+            }
+
+            if (som5 == Convert.ToInt16(txtUitkomst5.Text))
+            {
+                lblResultaat5.Text = "Goed";
+            }
+            else
+            {
+                lblResultaat5.Text = "Fout";
+                cijfer -= 2;
+            }
+
+            lblCijfer.Text = Convert.ToString(cijfer);
+
+        }
+    }
+}
